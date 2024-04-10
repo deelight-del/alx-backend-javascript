477e9220ae9eac35409fe85f67116fb60b89c157
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-10 15:10:59 +0000

    Task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoRes, userRes]) => {
+      console.log(`${photoRes.body} ${userRes.firstName} ${userRes.lastName}`);
+    })
+    .catch(() => console.error('Signup system offline'));
+}
