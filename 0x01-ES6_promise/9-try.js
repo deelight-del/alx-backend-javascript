d0fe1fb555d9ff7d85f5f59fb9b54ff01a8e67d4
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-10 17:18:11 +0000

    Format error message

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
     const queue = [mathFunction(), 'Guardrail was processed'];
     return queue;
   } catch (error) {
-    const queue = [error, 'Gurardrail was processed'];
+    const queue = [`${error.name}: ${error.message}`, 'Gurardrail was processed'];
     return queue;
   }
 }
