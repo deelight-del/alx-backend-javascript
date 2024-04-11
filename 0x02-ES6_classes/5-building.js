5b1f7e8bf4e9a8a0ec12f45edb7262c2d72b6ec2
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-11 19:16:58 +0000

    confused, task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,12 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor.name !== 'Building') {
+      if (this.hasOwnProperty('evacuationWarningMessage') === false) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() { return this._sqft; }
+}
