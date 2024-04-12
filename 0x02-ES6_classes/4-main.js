769475f0ac25b635a94a690ade5c1393dcdddab5
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-12 11:48:16 +0000

    task 7

--- 0x02-ES6_classes/4-main.js
+++ 0x02-ES6_classes/4-main.js
@@ -1,6 +1,11 @@
-import Pricing from './4-pricing.js';
 import Currency from './3-currency.js';
+import Pricing from './4-pricing.js';
 
-const p = new Pricing(100, new Currency('EUR', 'Euro'));
-console.log(p);
-console.log(p.displayFullPrice());
+const dollar = new Currency('$', 'Dollars');
+
+test('pricing is implemented correctly', () => {
+  const price = new Pricing(100, dollar);
+  expect(price.amount).toBe(100);
+  expect(price.currency).toBe(dollar);
+  expect(price.displayFullPrice()).toBe('100 Dollars ($)');
+});
