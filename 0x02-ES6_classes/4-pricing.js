073d24fc6760f18fba75a33db2fba91f60ad41c0
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-12 11:37:53 +0000

    task 4: fix by extending class

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,10 +1,10 @@
-/* eslint-disable */
 import Currency from './3-currency';
 
-export default class Pricing {
+export default class Pricing extends Currency {
   constructor(amount, currency) {
+    super(currency.code, currency.name);
     this._amount = amount;
-    this._currency = currency;
+    // this._currency = currency;
   }
 
   get amount() { return this._amount; }
@@ -15,7 +15,7 @@ export default class Pricing {
 
   set currency(value) { this._currency = value; }
 
-  displayFullPrice() { return `${this._amount} ${this._currency.name} ${this._currency.code}`; }
+  displayFullPrice() { return `${this._amount} ${this.name} (${this.code})`; }
 
   static convertPrice(amount, conversionRate) { return amount * conversionRate; }
 }
