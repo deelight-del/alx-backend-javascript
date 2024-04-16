a6c7f90a7fc8bc96963ef3776bf962c2a3cda8fb
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-16 16:08:46 +0000

    Task 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,8 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  weakMap.has(endpoint)
+    ? weakMap.set(endpoint, weakMap.get(endpoint) + 1)
+    : weakMap.set(endpoint, 0);
+  if (weakMap.get(endpoint) >= 5) { throw Error('Endpoint load is high'); }
+}
