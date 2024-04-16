86c63b7802204648a4b7aa609cbb78cecc0d4f90
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-16 15:50:41 +0000

    xxx: task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const array = new Int8Array(buffer);
-  try {
-    array[position] = value;
-    return buffer;
-  } catch (error) {
+  if (position < 0 || position >= length) {
     throw Error('Position outside range');
   }
+  const buffer = new ArrayBuffer(length);
+  const array = new Int8Array(buffer);
+  array[position] = value;
+  return buffer;
 }
