48a7ab28f8c012804ad35bf18411da2c6ccfa340
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-16 11:49:56 +0000

    task 4"

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(listOfStudents, city, newGrades) {
+  const newListOfStudents = listOfStudents.filter((obj) => obj.location === city).map(
+    (filterObj) => {
+      const gradeObj = newGrades.find((element) => element.studentId === filterObj.id);
+      if (gradeObj === undefined) {
+        return { ...filterObj, grade: 'N/A' };
+      }
+      return { ...filterObj, grade: gradeObj.studentId };
+    },
+  );
+  return newListOfStudents;
+}
