c6441f2102b4f292aefa321bee5f04cb9bc63718
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-03 17:25:43 +0000

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    console.log(`Your name is: ${chunk}`);
+  }
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
