6f9f1fa398e3c23c4b3fbdc23c97631d978bea3b
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-03 17:32:29 +0000

    using process.stdout.write

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    console.log(`Your name is: ${chunk}`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
