e741a9d067aa67b3cb4a35a0634fdebd156048f8
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-03 17:12:42 +0000

    Xxx

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,7 +2,8 @@
 // Function to displayMessage.
 
 function displayMessage(arg) {
-  process.stdout.write(`${arg}\n`);
+  // process.stdout.write(`${arg}\n`);
+  console.log(arg);
 }
 
 module.exports = displayMessage;
