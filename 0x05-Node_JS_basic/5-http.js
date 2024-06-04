cf434dae138d39499ced988873ea6b9607e866e7
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-04 15:37:07 +0000

    xxx

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,51 @@
+/**
+ * Creating a more complex Server
+ * Using the Node Interface.
+ */
+
+const { createServer } = require('http');
+const fs = require('fs');
+
+const app = createServer((req, res) => {
+  if (req.url === '/') { res.end('Hello Holberton School!'); }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    const dbName = process.argv[2];
+    fs.open(dbName, (err, fd) => {
+      if (err) { throw new Error('Cannot load the database'); }
+      fs.read(fd, (err, bytesRead, buffer) => {
+        if (err) { throw new Error('Cannot load the database'); }
+        const bulkData = buffer.toString('utf8', 0, bytesRead).trim();
+        // Convert bulkData to Array.
+        const dataArray = bulkData.split('\n').slice(1);
+        let totalStudents = 0;
+        const departments = {};
+        for (const line of dataArray) {
+          // if (line.length > 1) {
+          //  continue;
+          // }
+          totalStudents += 1;
+          const lineArray = line.split(',');
+          const field = lineArray[3];
+          const firstName = lineArray[0];
+          if (field in departments) {
+            departments[field].count += 1;
+            departments[field].listOfStudents.push(firstName);
+          } else {
+            departments[field] = { count: 1, listOfStudents: [firstName] };
+          }
+        }
+        res.write(`Number of students: ${totalStudents}\n`);
+        for (const field in departments) {
+          res.write(`Number of students in ${field}: ${departments[field].count}. List: ${departments[field].listOfStudents.join(', ')}\n`);
+        }
+        res.end();
+        // res.end(bulkData);
+      });
+    });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
