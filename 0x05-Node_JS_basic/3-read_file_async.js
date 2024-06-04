180bc4350669ef810f165b959bcbf614b79a9844
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-04 11:07:43 +0000

    task 3, without 2, yet

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const { open } = require('node:fs/promises');
+
+let fileHandle;
+let totalStudents = 0;
+const departments = {};
+async function countStudents (path) {
+  try {
+    fileHandle = await open(path);
+    for await (const line of fileHandle.readLines()) {
+      if (line.startsWith('firstname') === true) continue;
+      totalStudents += 1;
+      const data = line.split(',');
+      const dept = data.slice(-1);
+      if (dept in departments) {
+        departments[dept].count += 1;
+        departments[dept].listOfStudents.push(data[0]);
+      } else {
+        departments[dept] = { count: 1, listOfStudents: [data[0]] };
+      }
+    }
+    console.log(`Number of students: ${totalStudents}`);
+    for (const dept in departments) {
+      console.log(`Number of students in ${dept}: ${departments[dept].count}. List: ${departments[dept].listOfStudents.join(', ')}`);
+    }
+  } catch (err) {
+    console.log('Cannot load the database');
+    throw err;
+  } finally {
+    if (fileHandle) { await fileHandle.close(); }
+  }
+}
+
+module.exports = countStudents;
