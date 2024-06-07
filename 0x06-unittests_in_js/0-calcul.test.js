753af5ddff11bccbd3640f211bf743972b7d7960
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-07 06:47:03 +0000

    xxx

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -19,13 +19,21 @@ describe('CalculateNumber', function () {
     assert.equal(CalculateNumber(1.5, 3.7), 6);
   });
 
-  it('1.7 + 3 should return 5', function () {
-    assert.equal(CalculateNumber(1.7, 3), 5);
-  });
-  it('1.2 + 6 should return 5', function () {
-    assert.equal(CalculateNumber(1.2, 6), 7);
-  });
-  it('0.2 + 6 should return 5', function () {
-    assert.equal(CalculateNumber(0.2, 6), 6);
+  describe('For second number rounded', function () {
+    it('1.2 + 6 should return 5', function () {
+      assert.equal(CalculateNumber(1.2, 6), 7);
+    });
+    it('0.2 + 6 should return 5', function () {
+      assert.equal(CalculateNumber(0.2, 6), 6);
+    });
+    it('0.2 + 6 should return 5', function () {
+      assert.equal(CalculateNumber(0.2, 6), 6);
+    });
+    it('1.7 + 3 should return 5', function () {
+      assert.equal(CalculateNumber(1.7, 3), 5);
+    });
+    it('3.7 + 3 should return 7', function () {
+      assert.equal(CalculateNumber(3.7, 3), 7);
+    });
   });
 });
