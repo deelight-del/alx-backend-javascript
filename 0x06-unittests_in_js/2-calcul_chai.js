85e641951fccadf25ff0bd0fb747f587a82a4029
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-07 00:49:48 +0000

    task 2

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,20 @@
+/**
+ * A calculator for given
+ *a and b.
+ */
+
+function calculateNumber (type, a, b) {
+  const newA = Math.round(a);
+  const newB = Math.round(b);
+  switch (type) {
+    case 'SUM':
+      return newA + newB;
+    case 'SUBTRACT':
+      return newA - newB;
+    case 'DIVIDE':
+      if (b === 0) { return 'Error'; }
+      return newA / newB;
+  }
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,21 @@
+/**
+ * The test file for './1-calcul.js'
+ */
+
+const expect = require('chai').expect;
+const calculateNumber = require('./1-calcul');
+
+describe('CalculateNumber', function () {
+  it('SUM of 1.4 and 4.5 should give 6', function () {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+  it('DIFFERENCE of 4.5 from 1.4 should give -4', function () {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+  it('DIVIDE 1.4 by 4.5 should give 0.2', function () {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+  it('DIVIDE 1.4 by 0 should give Error', function () {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+});
